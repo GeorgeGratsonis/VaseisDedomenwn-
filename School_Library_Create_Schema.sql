c44468af31494cc9b14b53ebb0390f6b4834be28
antkountou <133799379+antkountou@users.noreply.github.com>
2023-05-18 14:14:54 +0300

    Update School_Library_Create_Schema.sql

--- School_Library_Create_Schema.sql
+++ School_Library_Create_Schema.sql
@@ -5,7 +5,7 @@ USE School_Library;
 -- Table 'Administrator'
 
 CREATE TABLE IF NOT EXISTS Administrator (
-	Admin_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    Admin_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
     First_Name VARCHAR(50) NOT NULL,
     Last_Name VARCHAR(50) NOT NULL,
     PRIMARY KEY (Admin_ID)
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS Administrator (
  -- Table 'School'
 
 CREATE TABLE IF NOT EXISTS School (
-	School_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    School_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
     Name VARCHAR(50) NOT NULL UNIQUE,
     Adress VARCHAR(50) NOT NULL,
     City VARCHAR(50) NOT NULL,
@@ -33,11 +33,11 @@ CREATE TABLE IF NOT EXISTS School (
  -- Table 'LibraryOperator'
 
 CREATE TABLE IF NOT EXISTS LibraryOperator (
-	LibraryOperator_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    LibraryOperator_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
     First_Name VARCHAR(50) NOT NULL,
     Last_Name VARCHAR(50) NOT NULL,
     Admin_ID INT UNSIGNED NOT NULL,
-	School_ID INT UNSIGNED NOT NULL UNIQUE,
+    School_ID INT UNSIGNED NOT NULL UNIQUE,
     PRIMARY KEY (LibraryOperator_ID),
     INDEX fk_LibraryOperator_Admin_idx (Admin_ID ASC),
     INDEX fk_LibraryOperator_School_idx (School_ID ASC),
@@ -56,13 +56,13 @@ CREATE TABLE IF NOT EXISTS LibraryOperator (
 -- Table 'User'
 
 CREATE TABLE IF NOT EXISTS User (
-	User_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    User_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
     Username VARCHAR(50) NOT NULL UNIQUE,
     Password VARCHAR(50) NOT NULL,
     First_Name VARCHAR(50) NOT NULL,
     Last_Name VARCHAR(50) NOT NULL,
     Role ENUM('Student', 'Professor') NOT NULL,
-	School_ID INT UNSIGNED NOT NULL,
+    School_ID INT UNSIGNED NOT NULL,
     LibraryOperator_ID INT UNSIGNED NOT NULL,
     PRIMARY KEY (User_ID),
     INDEX fk_User_School_idx (School_ID ASC),
@@ -82,7 +82,7 @@ CREATE TABLE IF NOT EXISTS User (
 -- Table 'Book'
 
 CREATE TABLE IF NOT EXISTS Book (
-	Book_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    Book_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
     Title VARCHAR(50) NOT NULL,
     Publisher VARCHAR(50) NOT NULL,
     ISBN VARCHAR(50) NOT NULL,
@@ -98,7 +98,7 @@ CREATE TABLE IF NOT EXISTS Book (
 -- Table 'Author'
 
 CREATE TABLE IF NOT EXISTS Author (
-	Author_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    Author_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
     First_Name VARCHAR(50) NOT NULL,
     Last_Name VARCHAR(50) NOT NULL,
     INDEX Author_idx (Author_ID),
@@ -108,7 +108,7 @@ CREATE TABLE IF NOT EXISTS Author (
 -- Table 'Category'
 
 CREATE TABLE IF NOT EXISTS Category (
-	Category_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    Category_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
     Name VARCHAR(50) NOT NULL UNIQUE,
     INDEX Category_idx (Category_ID),
     PRIMARY KEY (Category_ID)
@@ -117,11 +117,11 @@ CREATE TABLE IF NOT EXISTS Category (
 -- Table 'Borrowing'
 
 CREATE TABLE IF NOT EXISTS Borrowing (
-	Borrowing_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    Borrowing_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
     Borrowing_Date DATE NOT NULL,
     Return_Date DATE NOT NULL,
     Returned BOOLEAN DEFAULT FALSE,
-	User_ID INT UNSIGNED NOT NULL,
+    User_ID INT UNSIGNED NOT NULL,
     LibraryOperator_ID INT UNSIGNED NOT NULL,
     Book_ID INT UNSIGNED NOT NULL,
     PRIMARY KEY (Borrowing_ID),
@@ -138,7 +138,7 @@ CREATE TABLE IF NOT EXISTS Borrowing (
 	REFERENCES LibraryOperator (LibraryOperator_ID)
 	ON DELETE RESTRICT
 	ON UPDATE CASCADE,
-	CONSTRAINT fk_Borrowing_Book
+    CONSTRAINT fk_Borrowing_Book
 	FOREIGN KEY (Book_ID)
 	REFERENCES Book (Book_ID)
 	ON DELETE RESTRICT
@@ -148,10 +148,10 @@ CREATE TABLE IF NOT EXISTS Borrowing (
 -- Table 'Reservation'
 
 CREATE TABLE IF NOT EXISTS Reservation (
-	Reservation_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    Reservation_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
     Reservation_Date DATE NOT NULL,
     Cancelled BOOLEAN DEFAULT FALSE,
-	User_ID INT UNSIGNED NOT NULL,
+    User_ID INT UNSIGNED NOT NULL,
     LibraryOperator_ID INT UNSIGNED NOT NULL,
     Book_ID INT UNSIGNED NOT NULL,
     PRIMARY KEY (Reservation_ID),
@@ -178,11 +178,11 @@ CREATE TABLE IF NOT EXISTS Reservation (
 -- Table 'Review'
 
 CREATE TABLE IF NOT EXISTS Review (
-	Review_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    Review_ID INT UNSIGNED NOT NULL AUTO_INCREMENT,
     Text TEXT NOT NULL,
     Rating INT NOT NULL,
     Approved BOOLEAN DEFAULT FALSE,
-	User_ID INT UNSIGNED NOT NULL,
+    User_ID INT UNSIGNED NOT NULL,
     LibraryOperator_ID INT UNSIGNED NOT NULL,
     Book_ID INT UNSIGNED NOT NULL,
     PRIMARY KEY (Review_ID),
@@ -199,7 +199,7 @@ CREATE TABLE IF NOT EXISTS Review (
 	REFERENCES LibraryOperator (LibraryOperator_ID)
 	ON DELETE RESTRICT
 	ON UPDATE CASCADE,
-	CONSTRAINT fk_Review_Book
+    CONSTRAINT fk_Review_Book
 	FOREIGN KEY (Book_ID)
 	REFERENCES Book (Book_ID)
 	ON DELETE RESTRICT
@@ -209,49 +209,49 @@ CREATE TABLE IF NOT EXISTS Review (
 -- Table 'Book_Author'
 
 CREATE TABLE IF NOT EXISTS Book_Author (
-	Author_ID INT UNSIGNED NOT NULL,
+    Author_ID INT UNSIGNED NOT NULL,
     Book_ID INT UNSIGNED NOT NULL,
     PRIMARY KEY (Author_ID, Book_ID),
     INDEX fk_Book_Author_ID_idx (Book_ID ASC),
     CONSTRAINT fk_Author_ID
-    FOREIGN KEY (Author_ID)
-    REFERENCES Author (Author_ID)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE,
+    	FOREIGN KEY (Author_ID)
+    	REFERENCES Author (Author_ID)
+    	ON DELETE RESTRICT
+    	ON UPDATE CASCADE,
     CONSTRAINT fk_Book_Author_ID
-    FOREIGN KEY (Book_ID)
-    REFERENCES Book (Book_ID)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE
+    	FOREIGN KEY (Book_ID)
+    	REFERENCES Book (Book_ID)
+    	ON DELETE RESTRICT
+    	ON UPDATE CASCADE
 ) ENGINE = InnoDB;
 
 -- Table 'Book_Category'
 
 CREATE TABLE IF NOT EXISTS Book_Category (
-	Category_ID INT UNSIGNED NOT NULL,
-	Book_ID INT UNSIGNED NOT NULL,
-	PRIMARY KEY (Category_ID, Book_ID),
-	INDEX fk_Book_Category_ID_idx (Book_ID ASC),
-	CONSTRAINT fk_Category_ID
-    FOREIGN KEY (Category_ID)
-    REFERENCES Category (Category_ID)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE,
-	CONSTRAINT fk_Book_categoryCategory_ID
-    FOREIGN KEY (Book_ID)
-    REFERENCES Book (Book_ID)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE
+    Category_ID INT UNSIGNED NOT NULL,
+    Book_ID INT UNSIGNED NOT NULL,
+    PRIMARY KEY (Category_ID, Book_ID),	
+    INDEX fk_Book_Category_ID_idx (Book_ID ASC),
+    CONSTRAINT fk_Category_ID
+    	FOREIGN KEY (Category_ID)
+    	REFERENCES Category (Category_ID)
+    	ON DELETE RESTRICT
+    	ON UPDATE CASCADE,
+    CONSTRAINT fk_Book_categoryCategory_ID
+    	FOREIGN KEY (Book_ID)
+    	REFERENCES Book (Book_ID)
+    	ON DELETE RESTRICT
+    	ON UPDATE CASCADE
 ) ENGINE = InnoDB;
 
 -- Table 'School_Phone'
 
 CREATE TABLE IF NOT EXISTS School_Phone (
-	School_ID INT UNSIGNED NOT NULL,
-	Phone_Number CHAR(10) NOT NULL UNIQUE,
-	CONSTRAINT check_phone CHECK (Phone_Number RLIKE('[0-9]{10}')),
-	PRIMARY KEY (Phone_Number),
-	CONSTRAINT fk_School_ID
+    School_ID INT UNSIGNED NOT NULL,
+    Phone_Number CHAR(10) NOT NULL UNIQUE,
+    CONSTRAINT check_phone CHECK (Phone_Number RLIKE('[0-9]{10}')),
+    PRIMARY KEY (Phone_Number),
+    CONSTRAINT fk_School_ID
 	FOREIGN KEY (School_ID)
 	REFERENCES School (School_ID)
 	ON DELETE RESTRICT
