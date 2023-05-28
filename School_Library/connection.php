92452c1c365742664513faa07e010c9bf3de89c2
antkountou <133799379+antkountou@users.noreply.github.com>
2023-05-28 19:17:29 +0300

    Add files via upload

---
+++ School_Library/connection.php
@@ -0,0 +1,19 @@
+<?php
+   
+   function OpenCon(){
+      $dbhost = "localhost";
+      $dbuser = "root";
+      $dbpass = "";
+      $db = "school_library";
+      
+      $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+      
+      //echo "Connected successfully";
+      return $conn;
+   }
+   
+   function CloseCon($conn){
+      $conn -> close();
+   }  
+
+?>
